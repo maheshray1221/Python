f3dd45545581e659cac638ea1cc85fa265aa61a8
maheshray1221 <mr5832388@gmail.com>
2025-09-12 21:24:38 +0530

    add file

---
+++ 01_dataType/chepter_1.py
@@ -0,0 +1,71 @@
+# -------------  immutable  ------------------
+# count = 1
+# print(f"hello count : {count}")
+# count = 5
+# print(f"namaste count : {count}")
+# print(f"identity 1 : {id(1)}")
+# print(f"identity 5 : {id(5)}")
+
+
+# ----------------  Mutable  ------------------
+
+# a = set()
+# print(f"id of a :{id(a)}")
+# a.add("hero")
+# a  .add("kalu")
+# print(f"id of a : {id(a)}")
+
+# --------------- Number ----------------
+# adding
+# a = 2
+# b = 4
+# print(f"result of adding : {a+b}")
+# #Subtact
+# c = 8
+# d = 3
+# #multiple
+# print(f"multiple of this : {c*d}")
+# # power
+# print(f"power of this : {b**a}")
+
+# print(f"subtract of this : {c-d}")
+# #devid
+# print(f"devid of this : {c/d}")
+# #double devid
+# print(f"double devid of this : {c//d}")
+# #remender - module
+# print(f"module of this : {c%d}")
+
+#--------------- Boolean --------------
+# a = 1
+# b = True
+# c = 0  # 0,nane
+# result = a+b
+# print(f"add : {result}")
+# #bool
+# print(f"type of :{bool(a)}")
+# print(f"type of :{bool(c)}")
+
+#-------------- logical operator -------------
+
+# a = True
+# b = False
+# print(f"result : {a and b}") #false
+# print(f"result : {a or b}") #true
+
+#--------------  sys ----------
+
+# import sys
+# print(sys.float_info)
+
+#--------------  String  -----------
+# first_name = "Mahesh"
+# last_name = "Ray"
+# full_name = first_name+last_name
+# print(f"result : {full_name}")
+
+#-------------- indexing ---------------
+# name = "vivekanand is a great person"
+# print(f"first_name {name[21:]}")
+# print(f"first_name {name[:10]}")
+# print(f"first_name {name[::-1]}")
